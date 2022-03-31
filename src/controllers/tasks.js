1db5cf2fcc172077c733af44ddc372179cfc8193
Oghenekparobo Onosemuode <kparaboj@gmail.com>
2022-03-31 08:23:24 +0100

    CRUD operations

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,18 +1,34 @@
 var Task = require("../modules/task");
 
-const getAllTasks = (req,res) => {
-    res.send("get all tasks");
+const getAllTasks = async (req,res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks});
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+    // res.send("get all tasks");
     // next();
-}
+};
 
-const getTask = (req,res) => {
-    res.json({
-        "message": "update task",
-        "task_id": req.params.id
-    });
-    // next();
-}
+const getTask = async (req,res) => {
+    try {
+        const { id:taskID } = req.params;
+        const task = await Task.findOne({_id:taskID}).exec();
+        res.status(200).json({ task });
+        if(!task){
+            return res.status(400).json( {error: 'No task with id: ${taskID}'});
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+};
 
+//Create operation
 const createTask = async (req,res) => {
     try {
         const task = await Task.create(req.body);
@@ -22,17 +38,17 @@ const createTask = async (req,res) => {
             error: err
         });
     }
-}
+};
 
 const updateTask = (req,res) => {
     res.send("update single task");
     // next();
-}
+};
 
 const deleteTask = (req,res) => {
     res.send("delete task");
     // next();
-}
+};
 
 
 module.exports = {
